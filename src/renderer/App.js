2f71a15a001833da6ceb0755bbd703d9d055d51d
roytan883 <blueness883@163.com>
2018-05-08 01:38:31 +0800

    can use material-ui

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import { Helmet } from "react-helmet";
 
-import { Button, Icon } from 'antd-mobile';
+import IconButton from 'material-ui/IconButton';
+import Button from 'material-ui/Button';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+import { Button as BtnA, Icon } from 'antd-mobile';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        hello world
-        <Button type="primary" size="small" inline> Click </Button>
+        <Helmet>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Helmet>
+
+        hello world 1
+        <BtnA type="primary" size="small" inline> Click </BtnA>
         <Icon type="search" size="lg" />
+        <Button variant="raised" color="primary">
+          M
+        </Button>
+        <IconButton color="secondary" component="span">
+          <PhotoCamera />
+        </IconButton>
       </div>
     )
   }
