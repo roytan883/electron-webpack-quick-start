4c633691d01934c66a96598fd424719e630e400f
roytan883 <blueness883@163.com>
2018-05-21 18:29:21 +0800

    nav

--- src/web-admin/pages/Nav.jsx
+++ src/web-admin/pages/Nav.jsx
@@ -48,25 +48,25 @@ class NavSider extends React.Component {
           <Icon type="pie-chart" />
           <span>首页</span>
         </Menu.Item>
-        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-          <Menu.Item key="page1">页面1</Menu.Item>
-          <Menu.Item key="page2">页面2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
+        <SubMenu key="User" title={<span><Icon type="mail" /><span>用户管理</span></span>}>
+          <Menu.Item key="UserAdmin">管理员</Menu.Item>
+          <Menu.Item key="UserNormal">用户</Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigtion Two</span></span>}>
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
+        <SubMenu key="Day" title={<span><Icon type="appstore" /><span>每日挑战</span></span>}>
+          <Menu.Item key="DaySetting">设置</Menu.Item>
+          <Menu.Item key="DayText">正文</Menu.Item>
+          <Menu.Item key="DayExamLib">题库</Menu.Item>
+          <Menu.Item key="DayResult">结果</Menu.Item>
         </SubMenu>
         <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
           <Menu.Item key="9">Option 9</Menu.Item>
           <Menu.Item key="10">Option 10</Menu.Item>
           <Menu.Item key="11">Option 11</Menu.Item>
           <Menu.Item key="12">Option 12</Menu.Item>
+          <SubMenu key="sub3" title="每日题库">
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
         </SubMenu>
       </Menu>
     );
